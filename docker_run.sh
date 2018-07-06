00376889ec746a789ab07bdb8e48efaed2cb3f01
vhborges <viniciusborges22@gmail.com>
2018-07-05 22:29:15 -0300

    add docker_run script

---
+++ docker_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION="Release"
+
+CONTAINER_NAME="tdesktop-linux"
+IMAGE_NAME="telegramdesktop/linux_build"
+
+BIN_PATH="/TBuild/tdesktop/out/Release/Telegram"
+DEST_PATH="binaries/"
+
+docker run --name $CONTAINER_NAME $IMAGE_NAME 
+
+docker cp ${CONTAINER_NAME}:${BIN_PATH} $DEST_PATH
